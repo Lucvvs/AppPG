1d4e9d12c1dd915667e71fee54f04cf7a4342da8
Lucvvs <lucas.olivos33@gmail.com>
2025-06-16 17:33:45 -0400

    V43

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'AppPG',
-  webDir: 'www',
-  server: {
-    url: 'http://192.168.100.9:8100',
-    cleartext: true
-  }
+  webDir: 'www'
 };
 
 export default config;
